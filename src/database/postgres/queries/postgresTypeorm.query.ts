2550d5caf5cc7bf30ea746b404a3dd98d0ac951a
data-spartan <data.spartaan@gmail.com>
2023-11-06 19:27:10 +0100

    [IB-18] Fixed query runner

--- src/database/postgres/queries/postgresTypeorm.query.ts
+++ src/database/postgres/queries/postgresTypeorm.query.ts
@@ -1,13 +1,17 @@
 import { EntityClassOrSchema } from '@nestjs/typeorm/dist/interfaces/entity-class-or-schema.type';
 import { DataSource, Entity, QueryRunner } from 'typeorm';
 import { User } from 'src/api/users/index.entity';
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { PostgresTypeOrmRawQueries } from '../raw-queries/postgresTypeorm.rawqueries';
 
 @Injectable()
 export class PostgresTypeOrmQueries {
-  private readonly queryRunner: QueryRunner;
-  constructor(private dataSource: DataSource) {
-    this.queryRunner = this.dataSource.createQueryRunner();
+  // private readonly queryRunner: QueryRunner;
+  constructor(
+    private dataSource: DataSource,
+    private rawQueries: PostgresTypeOrmRawQueries,
+  ) {
+    // this.queryRunner = this.dataSource.createQueryRunner();
   }
   public async allUsersPagination(
     //in cursor pag with uuid, date/timestamp is in fact cursor and uuid is here to compare which date is greater/smaller when dates are equal
@@ -23,18 +27,52 @@ export class PostgresTypeOrmQueries {
     //if Next page is clicked, FE needs to send last record(oldest) in  user array; Send CreatedAt, id, direction(Next) from previous array
     //to able to to show older records than CreatedAt
     //when Previous is clicked first record in array is sent.
-    const columns = `"id","firstName", "lastName", "telephone","email","verifiedEmail","role","createdAt","updatedAt","lastLoginAt"`;
-    const query = `
-    SELECT ${columns}
-    FROM public.users
-    WHERE ("createdAt", "id") ${sign} ($1, $2)
-    ORDER BY "createdAt" DESC
-    LIMIT $3;
-  `;
-    //WHERE ... row constructor -> first checks created_at < x, unless these values are equal, in which case it compares id and y`.
-    await this.queryRunner.connect();
-    const users = await this.queryRunner.query(query, [cursor, userId, limit]);
-    await this.queryRunner.release();
+
+    const queryRunner = this.dataSource.createQueryRunner();
+    const rawQuery = await this.rawQueries.allUsersPagination(sign);
+    await queryRunner.connect();
+    const users = await queryRunner.query(rawQuery, [cursor, userId, limit]);
+    await queryRunner.release();
     return users;
+  }
+
+  public async refreshTokenTransaction(
+    entity: EntityClassOrSchema,
+    hashedRefreshToken: string,
+    propertyObject: Object,
+    id: string,
+  ) {
+    // const queryRunner: QueryRunner = this.dataSource.createQueryRunner();
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+    const manager = queryRunner.manager;
+    try {
+      await manager.delete(entity, propertyObject); //{ user: id }
+      const insertResult = await manager.insert(entity, {
+        refreshToken: hashedRefreshToken,
+        user: id,
+      });
+      await queryRunner.commitTransaction();
+      return insertResult;
+    } catch (error) {
+      await queryRunner.rollbackTransaction().catch((error: Error) => {
+        throw error;
+      });
+      throw new InternalServerErrorException(error);
+    } finally {
+      await queryRunner.release();
+    }
+
+    // const insertResult = await this.dataSource.manager.transaction(
+    //   async (transEntMan) => {
+    //     await transEntMan.delete(entity, propertyObject); //{ user: id }
+    //     const insertResult = await transEntMan.insert(entity, {
+    //       refreshToken: hashedRefreshToken,
+    //       user: id,
+    //     });
+    //     return insertResult;
+    //   },
+    // );
   }
 }
